a25d5116a3aeb4029735d4198574c809f11308f5
Elradave <115457672+Elradave@users.noreply.github.com>
2023-04-10 15:35:39 +0100

    Delete userlogin directory

--- userlogin/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('sign_up', views.sign_up, name='sign_up'),
-    path('create-post', views.createpost, name='create-post'),
-    path('profile', views.profile, name='profile')
-]
