1b22ffd4ce9a95816f613d7537a1ae939dba84ec
Elradave <segunreal1@gmail.com>
2023-04-13 16:26:40 +0100

    Update urls.py

--- userlogin/main/urls.py
+++ userlogin/main/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('oldest', views.home, name='oldest'),
+    path('latest', views.home, name='latest'),
     path('sign_up', views.sign_up, name='sign_up'),
     path('create-post', views.createpost, name='create-post'),
     path('profile/<str:user_id>', views.profile, name='profile'),
